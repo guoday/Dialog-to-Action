5684f41591686734078d7acdff3310183b6a13e7
guoday <guoday5@mail2.sysu.edu.cn>
2019-07-27 06:25:32 -0700

    update

--- data/download.sh
+++ data/download.sh
@@ -2,6 +2,7 @@ wget https://zenodo.org/record/3268649/files/CSQA_v9.zip
 wget https://zenodo.org/record/3267282/files/wikidata_proc_json-20190701T093606Z-001.zip
 unzip CSQA_v9.zip
 rm -r CSQA_v9.zip
+mv CSQA_v9 CSQA
 unzip wikidata_proc_json-20190701T093606Z-001.zip
 rm -r wikidata_proc_json-20190701T093606Z-001.zip
 mv wikidata_proc_json kb
