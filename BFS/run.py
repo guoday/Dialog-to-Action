56ac2ee9b1fd527146993cb371b226d3718479b9
Daya Guo <40300434+guoday@users.noreply.github.com>
2020-06-07 20:41:16 +0800

    Update run.py

--- BFS/run.py
+++ BFS/run.py
@@ -2,6 +2,7 @@
 #-*-coding=utf-8-*-  
 import sys
 import os
+sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
 import json
 from nltk.tokenize import WordPunctTokenizer
 import numpy as np
